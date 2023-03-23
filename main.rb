8be5441c60857783b11ef7f7e00fec4a9ebf6f77
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-03-22 22:19:53 -0400

    figure out how to keep comparing and swapping until everything is in order

---
+++ main.rb
@@ -0,0 +1,20 @@
+def bubble_sort(array)
+
+  length = array.length
+
+  loop do
+
+    (length-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+    break if not swapped
+  end
+
+  array
+end
+
+p bubble_sort([4,3,78,2,0,2])
+# [0,2,2,3,4,78]
