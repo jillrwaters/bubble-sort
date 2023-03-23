595bbe77cce10819a12d5d16d3d4012e1abf968e
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-03-23 10:52:51 -0400

    fixed

--- main.rb
+++ main.rb
@@ -3,14 +3,14 @@ def bubble_sort(array)
   length = array.length
 
   loop do
-
+    swapped = false
     (length-1).times do |i|
       if array[i] > array[i+1]
         array[i], array[i+1] = array[i+1], array[i]
         swapped = true
       end
     end
-    break if not swapped
+    break if !swapped
   end
 
   array
